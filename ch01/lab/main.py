828fb03d41212527f4b38d7ff5023e38bd75f93a
srirara <sbautis1@binghamton.edu>
2023-01-30 17:02:43 -0500

    ch1 exercise completed

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,12 @@
+#Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
+
+classes_per_week = (classes / weeks)
+cost_per_class = (cost_per_week / classes_per_week)
+print("Cost per class:", cost_per_class)
+
 
