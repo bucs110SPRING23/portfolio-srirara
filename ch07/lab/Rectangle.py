c0d6cc830a2b673ec0f48edc4d4247852f111bb0
srirara <sbautis1@binghamton.edu>
2023-03-30 20:47:15 -0400

    lab 7 complete

---
+++ ch07/lab/Rectangle.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, x, y, h, w):
+        self.x = abs(x)
+        self.y = abs(y)
+        self.height = abs(h)
+        self.width = abs(w)
+    def __str__(self):
+        ''' 
+        returns a string of the instance variables
+            args: self
+            returns: str
+        '''
+        return f"(x: {self.x}, y: {self.y}), width: {self.width}, height: {self.height}"
