5c01e3c991f8643fa345b98eddcf7c906f70f462
srirara <sbautis1@binghamton.edu>
2023-03-30 20:49:01 -0400

    added doc strings

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     def __str__(self):
         ''' 
         returns a string of the instance variables
-            args: self
+            args: self (Rectangle)
             returns: str
         '''
         return f"(x: {self.x}, y: {self.y}), width: {self.width}, height: {self.height}"
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -4,4 +4,9 @@ class Surface:
         self.image = filename
         self.rect = Rectangle(x, y, h, w)
     def getRect(self):
+        '''
+        returns the Rectangle attribute of a Surface
+            args: self (Surface)
+            returns: self.rect (Rectangle)
+        '''
         return self.rect
